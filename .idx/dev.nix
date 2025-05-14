abf5f21444f1ca7997b7b2ae9f58a7b36a069165
Envios Dosruedas <enviosdosruedas@gmail.com>
2025-05-14 03:03:08 +0000

    quizas deberiamos ajustar .idx/dev.nix del proyecto para que el inicio en   workspace = {       onCreate = {         default.openFiles = [           "src/app/page.tsx"         ];       };     }; no sea asi ya que estoy usando el /test-login para que ingresen los usuario

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,37 +1,98 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
-{pkgs}: {
+{ pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.11"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
-  packages = [
-    pkgs.nodejs_20
-    pkgs.zulu
-    pkgs.python314
-  ];
-  # Sets environment variables in the workspace
+  channel = "stable-23.11"; # Or "unstable"
+  # Use https://idx.dev/reference/common-modules to add attributes.
+  common = {
+    # Enabled language support for javascript, typescript, and nodejs.
+    javascript.enable = true;
+    typescript.enable = true;
+    nodejs.enable = true;
+    # Enabled postgresql.
+    services.postgresql.enable = true;
+  };
+  # Environment variables.
   env = {};
+  # Search for packages using https://search.nixos.org/packages
+  packages = [
+    pkgs.nodejs_22
+    pkgs.nodePackages.pnpm
+    pkgs.nodePackages.firebase-tools
+    pkgs.supabase-cli
+  ];
+  # Defines a script to run when a workspace is created.
+  onCreate = {
+    # Example: Adding a file to a workspace.
+    # test-success = ''
+    #   # Name of the file to create.
+    #   filename="test-success.txt"
+    #   # Content of the file.
+    #   content="Hello from onCreate hook!"
+    #   # Create the file.
+    #   echo "$content" > "$filename"
+    # '';
+  };
+  # Defines a script to run when a workspace is started.
+  onStart = {
+    # Example: Publishing a port.
+    # server = {
+    #   # Script to run.
+    #   command = "npm run dev";
+    #   # Name of the port to publish.
+    #   name = "server";
+    #   # Port number to publish.
+    #   port = 3000;
+    #   # Description of the port.
+    #   description = "Web server for the application";
+    #   # URL to open when the port is ready.
+    #   open = "http://localhost:3000";
+    # };
+  };
+  # Pinned Nixpkgs version.
+  # Determined by the channel selected via `common.channel` option.
+  nixpkgs = {
+    # rev = "52e3e80afff4b16cc1c190e0950a9a79000ac065";
+    # sha256 = "15g9ng7bg2v4Nl9i3wsc9v44zb30b6796k86fjn8m3zji9l30n80";
+  };
+  # IDX customizations.
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    # The list of extensions to recommend to the user.
+    # For more details, see https://idx.dev/reference/extensions
     extensions = [
-      # "vscodevim.vim"
+      "dbaeumer.vscode-eslint"
+      # "esbenp.prettier-vscode"
     ];
     workspace = {
+      # Runs when a workspace is first created.
       onCreate = {
         default.openFiles = [
-          "src/app/page.tsx"
+          "src/app/test-login/page.tsx"
         ];
       };
-    };
-    # Enable previews and customize configuration
-    previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
-          manager = "web";
-        };
+      # Runs when a workspace is (re)started.
+      onStart = {
+        # Example: Start a background task.
+        # npm-install = "npm install";
       };
     };
+    # Preview environments.
+    previews = {
+      # Enable previews by default.
+      enable = true;
+      # Include a script to run when a preview environment is created.
+      onCreate = {
+        # Example: Adding a file to a preview environment.
+        # test-success = ''
+        #   # Name of the file to create.
+        #   filename="test-success.txt"
+        #   # Content of the file.
+        #   content="Hello from onCreate hook!"
+        #   # Create the file.
+        #   echo "$content" > "$filename"
+        # '';
+      };
+    };
+    # List of processes to run.
+    # For more details, see https://idx.dev/reference/processes
+    processes = {};
   };
 }
